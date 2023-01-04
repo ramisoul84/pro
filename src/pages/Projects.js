2062a990bc3569f736a20e417002837e74d77e28
ramis <111607770+ramisoul84@users.noreply.github.com>
2023-01-04 18:13:58 +0300

    project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,8 +2,8 @@ const Projects = () => {
   return (
     <section>
       <div className="title">
-        <h5>Hello I'm</h5>
-        <h2>Rami Suliman</h2>
+        <h5>some of</h5>
+        <h2>My Projects</h2>
       </div>
       <h2>Soon</h2>
     </section>
